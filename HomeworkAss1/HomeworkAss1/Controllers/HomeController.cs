b5cf2e80bc61f0342cab04a6ad969393852f74aa
MatC <matcomtois@gmail.com>
2022-04-25 18:35:39 -0700

    Initial commit

---
+++ HomeworkAss1/HomeworkAss1/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using HomeworkAssign1.Models;
+using HomeworkAss1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HomeworkAss1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult BirthdayCardForm()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Thanks()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BirthdayCardForm(BirthdayCard birthdayCard)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", birthdayCard);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ HomeworkAss1/HomeworkAss1/Models/BirthdayCard.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeworkAssign1.Models
+{
+    public class BirthdayCard
+    {
+        [Required(ErrorMessage = "Please enter a From")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter a To")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Message")]
+        public string Message { get; set; }
+
+    }
+}
